94da908e3a34797257e62c953e1999a2fb0342ee
JHEAminoff <narqulie@gmail.com>
2026-05-28 13:48:14 +0300

    update Homebrew formula to v0.3.0

--- stickynote.rb
+++ stickynote.rb
@@ -1,8 +1,8 @@
 class Stickynote < Formula
   desc "Terminal-based sticky notes board with markdown, tags, and mouse support"
   homepage "https://github.com/Narqulie/stickynote"
-  url "https://github.com/Narqulie/stickynote/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "2fae944f85f168b03379bc724a14063a69052075fb14ae1489f7ee82a8104e16"
+  url "https://github.com/Narqulie/stickynote/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "350e7948f9faf132ed810936b085c6ebf5d8bb7fef2864e1485895d36fd37b5a"
   license "MIT"
 
   depends_on "rust" => :build
